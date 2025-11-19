c88f66332335b797e0a2c6fcfc1a9890ab0ac623
DoraGarcia <a01541311@tec.mx>
2025-11-18 20:00:16 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+export default function App() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-600">
+      Tailwind está funcionando 🎉
+    </h1>
+  );
+}
+
